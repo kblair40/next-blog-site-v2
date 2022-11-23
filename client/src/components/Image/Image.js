d482cad7ba472068cee2db725262eb2a72637f85
Kevin Blair <kblair40@gmail.com>
2022-11-23 08:13:12 -0600

    Add Image component/fix import paths in SEO component

---
+++ client/src/components/Image/Image.js
@@ -0,0 +1,20 @@
+import NextImage from "next/image";
+
+import { getStrapiMedia } from "src/lib/media";
+
+const Image = ({ image }) => {
+  const { alternativeText, width, height } = image.data.attributes;
+
+  return (
+    <NextImage
+      layout="responsive"
+      width={width}
+      height={height}
+      objectFit="contain"
+      src={getStrapiMedia(image)}
+      alt={alternativeText || ""}
+    />
+  );
+};
+
+export default Image;
