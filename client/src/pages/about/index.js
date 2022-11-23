4009362f557b0e1835db133047e94fbd7042936e
Kevin Blair <kblair40@gmail.com>
2022-11-23 16:56:26 -0600

    Add placeholder pages for About and Contact

---
+++ client/src/pages/about/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "@chakra-ui/react";
+
+const AboutPage = () => {
+  return (
+    <Text mt="2rem" textAlign="center">
+      Todo: About Page
+    </Text>
+  );
+};
+
+export default AboutPage;
---
+++ client/src/pages/contact/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "@chakra-ui/react";
+
+const ContactPage = () => {
+  return (
+    <Text mt="2rem" textAlign="center">
+      Todo: Contact Page
+    </Text>
+  );
+};
+
+export default ContactPage;
