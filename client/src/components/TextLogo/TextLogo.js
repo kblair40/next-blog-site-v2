98a32b17cc05ae8115a9e8675745b8c83628f81c
Kevin Blair <kblair40@gmail.com>
2022-11-24 13:16:35 -0600

    Make main logo a link to home

--- client/src/components/TextLogo/TextLogo.js
+++ client/src/components/TextLogo/TextLogo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Flex, Text, Heading, useBreakpointValue } from "@chakra-ui/react";
+import Link from "next/link";
 
 const TextLogo = () => {
   const isMd = useBreakpointValue({ base: false, md: true });
@@ -19,18 +20,20 @@ const SmallLogo = () => {
       borderBottom="1px solid #303030"
       pb="1rem"
     >
-      <Heading
-        maxW="90vw"
-        color="brand.darkgreen"
-        fontFamily="Playfair Display"
-        textAlign="center"
-        fontSize={{ base: "36px", sm: "42px" }}
-        lineHeight="normal"
-        mb="10px"
-        fontWeight="800"
-      >
-        Money and Other Things
-      </Heading>
+      <Link href="/">
+        <Heading
+          maxW="90vw"
+          color="brand.darkgreen"
+          fontFamily="Playfair Display"
+          textAlign="center"
+          fontSize={{ base: "36px", sm: "42px" }}
+          lineHeight="normal"
+          mb="10px"
+          fontWeight="800"
+        >
+          Money and Other Things
+        </Heading>
+      </Link>
       <Text color="brand.darkgreen" fontSize={{ sm: "17px" }}>
         A collection of money stories and tips
       </Text>
@@ -47,19 +50,21 @@ const LargeLogo = () => {
       mb={{ base: "28px", md: "58px" }}
       h="180px"
     >
-      <Heading
-        maxW="90vw"
-        color="brand.darkgreen"
-        fontFamily="Playfair Display"
-        textAlign="center"
-        fontSize={{ md: "60px", lg: "72px" }}
-        whiteSpace="nowrap"
-        lineHeight="normal"
-        mb="10px"
-        fontWeight="800"
-      >
-        Money and Other Things
-      </Heading>
+      <Link href="/">
+        <Heading
+          maxW="90vw"
+          color="brand.darkgreen"
+          fontFamily="Playfair Display"
+          textAlign="center"
+          fontSize={{ md: "60px", lg: "72px" }}
+          whiteSpace="nowrap"
+          lineHeight="normal"
+          mb="10px"
+          fontWeight="800"
+        >
+          Money and Other Things
+        </Heading>
+      </Link>
       <Text color="brand.darkgreen" fontSize={{ sm: "17px" }}>
         A collection of money stories and tips
       </Text>
