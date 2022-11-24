e1af39fbc17bebb2c11b423893539ab52fde7a16
Kevin Blair <kblair40@gmail.com>
2022-11-24 07:11:27 -0600

    ADd filter to api fetch to only return posts by selected category

--- client/src/pages/posts/[category].js
+++ client/src/pages/posts/[category].js
@@ -13,39 +13,68 @@ const Posts = ({ articles, categories }) => {
     // article: true,
   };
 
-  return (
-    <Fragment>
-      <Flex justify="center">
-        <Stack mt="2rem" spacing="1.5rem">
-          {articles && articles.length
-            ? articles.map((article, i) => {
-                return <Card article={article} key={i} />;
-              })
-            : null}
-        </Stack>
-      </Flex>
-    </Fragment>
-  );
+  console.log("ARTICLES/CATEGORIES:", { articles, categories });
+
+  return null;
+  // return (
+  //   <Fragment>
+  //     <Flex justify="center">
+  //       <Stack mt="2rem" spacing="1.5rem">
+  //         {articles && articles.length
+  //           ? articles.map((article, i) => {
+  //               return <Card article={article} key={i} />;
+  //             })
+  //           : null}
+  //       </Stack>
+  //     </Flex>
+  //   </Fragment>
+  // );
 };
 
 export async function getStaticProps({ params }) {
   console.log("PARAMS:", params);
+  const category = params.category;
+  console.log("CATEGORY:", category);
   const articlesRes = await fetchAPI("/articles", {
-    // filters: {
-    //   category: params.category,
-    // },
     populate: "*",
   });
-  const categoriesRes = await fetchAPI("/categories");
+  const categoriesRes = await fetchAPI("/categories", {
+    filters: {
+      name: category,
+    },
+    populate: "*",
+    // populate: {
+    //   articles: {
+    //     where: {
+    //       name: { $eq: category },
+    //     },
+    //   },
+    // },
+  });
 
   console.log("n\nARTICLES RES:", articlesRes);
+  console.log("n\nCATEGORIES RES:", categoriesRes);
 
   return {
     props: { articles: articlesRes.data, categories: categoriesRes },
-    // props: { article: articlesRes.data[0], categories: categoriesRes },
-    // revalidate: 1,
+    revalidate: 1,
   };
 }
+
+// export async function getStaticProps({ params }) {
+//   console.log("PARAMS:", params);
+//   const articlesRes = await fetchAPI("/articles", {
+//     populate: "*",
+//   });
+//   const categoriesRes = await fetchAPI("/categories");
+
+//   console.log("n\nARTICLES RES:", articlesRes);
+
+//   return {
+//     props: { articles: articlesRes.data, categories: categoriesRes },
+//     revalidate: 1,
+//   };
+// }
 
 export async function getStaticPaths() {
   // let articlesRes;
