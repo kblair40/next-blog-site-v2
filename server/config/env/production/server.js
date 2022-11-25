57d21b45fba747c6ccddde3e737e0bdfd137854c
Kevin Blair <kblair40@gmail.com>
2022-11-25 11:29:55 -0600

    update env config

--- server/config/env/production/server.js
+++ server/config/env/production/server.js
@@ -1,7 +1,5 @@
 module.exports = ({ env }) => ({
-  proxy: true,
-  url: env("APP_URL"), // replaces `host` and `port` properties in the development environment
-  app: {
-    keys: env.array("APP_KEYS"),
-  },
+  url: env("MY_HEROKU_URL"),
 });
+
+// --app money-and-other-things
