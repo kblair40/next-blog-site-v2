2097078a3e6431f1de47961c0e1fa753e81ee8b3
Kevin Blair <kblair40@gmail.com>
2022-11-26 11:34:49 -0600

    update configs

--- client/next.config.js
+++ client/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   images: {
     loader: "default",
     // hostname: "localhost",
-    hostname: "money-and-other-things.herokuapp.com",
+    // hostname: "money-and-other-things.herokuapp.com",
     // domains: ["localhost"],
     domains: ["money-and-other-things.herokuapp.com"],
   },
