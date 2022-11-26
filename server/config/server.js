edface08657011b02bb922628028e040337b2525
Kevin Blair <kblair40@gmail.com>
2022-11-25 18:46:28 -0600

    Update default server config

--- server/config/server.js
+++ server/config/server.js
@@ -1,7 +1,21 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
+  // host: env("HOST", "0.0.0.0"),
+  // port: env.int("PORT", 1337),
+  url: env("MY_HEROKU_URL"),
   app: {
     keys: env.array("APP_KEYS"),
   },
 });
+
+// module.exports = ({ env }) => ({
+//   url: env("MY_HEROKU_URL"),
+// });
+
+// ORIGINAL
+// module.exports = ({ env }) => ({
+//   host: env("HOST", "0.0.0.0"),
+//   port: env.int("PORT", 1337),
+//   app: {
+//     keys: env.array("APP_KEYS"),
+//   },
+// });
