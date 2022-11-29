41ef49e3c63358f26bb05bf7909e5832f672cfb8
Kevin Blair <kblair40@gmail.com>
2022-11-29 14:41:19 -0600

    Remove seo prop

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -5,7 +5,7 @@ import MobileNav from "src/components/Navbar/MobileNav";
 import Navbar from "src/components/Navbar";
 import TextLogo from "src/components/TextLogo";
 
-const layout = ({ children, categories, seo }) => {
+const layout = ({ children, categories }) => {
   return (
     <Box overflowX="hidden" minH="100vh" id="layout">
       <Box display={{ base: "none", md: "block" }}>
@@ -19,7 +19,9 @@ const layout = ({ children, categories, seo }) => {
         <TextLogo />
       </Box>
 
-      <Box position="relative">{children}</Box>
+      <Box h="100%" border="1px solid green" position="relative">
+        {children}
+      </Box>
 
       <Text fontSize="sm" mt="3rem" mb="1rem" textAlign="center">
         &copy; 2022 moneyandotherthings.com
