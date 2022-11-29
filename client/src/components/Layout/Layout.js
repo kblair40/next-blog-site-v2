c6d3bf061916f6476a02fb642dada5d113a3e433
Kevin Blair <kblair40@gmail.com>
2022-11-29 14:38:59 -0600

    Add copyright notice

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 
 import MobileNav from "src/components/Navbar/MobileNav";
 import Navbar from "src/components/Navbar";
@@ -20,6 +20,10 @@ const layout = ({ children, categories, seo }) => {
       </Box>
 
       <Box position="relative">{children}</Box>
+
+      <Text fontSize="sm" mt="3rem" mb="1rem" textAlign="center">
+        &copy; 2022 moneyandotherthings.com
+      </Text>
     </Box>
   );
 };
