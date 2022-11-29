b3fd1dc4b193e34cb5816ff9ac61504b7c23a140
Kevin Blair <kblair40@gmail.com>
2022-11-29 16:00:35 -0600

    Configure google analytics

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,11 +1,15 @@
 import App from "next/app";
 import Head from "next/head";
 import { ChakraProvider } from "@chakra-ui/react";
+import ReactGA from "react-ga";
 
 import { fetchAPI } from "src/utils/api";
 import theme from "src/utils/theme";
 import Layout from "src/components/Layout";
 import "../assets/css/style.css";
+
+const TRACKING_ID = "UA-250380145-1";
+ReactGA.initialize(TRACKING_ID);
 
 const MyApp = ({ Component, pageProps }) => {
   return (
