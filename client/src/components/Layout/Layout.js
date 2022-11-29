9aae3774cb6d2594c28df7ad64f60f7296fe7c51
Kevin Blair <kblair40@gmail.com>
2022-11-29 14:51:50 -0600

    Refactor layout so copyright notice is always at bottom

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Text, Flex } from "@chakra-ui/react";
 
 import MobileNav from "src/components/Navbar/MobileNav";
 import Navbar from "src/components/Navbar";
@@ -7,24 +7,38 @@ import TextLogo from "src/components/TextLogo";
 
 const layout = ({ children, categories }) => {
   return (
-    <Box overflowX="hidden" h="100%" minH="100vh" id="layout">
-      <Box display={{ base: "none", md: "block" }}>
-        <TextLogo />
-        <Navbar categories={categories} />
-      </Box>
+    <Flex
+      overflowX="hidden"
+      h="100%"
+      minH="100vh"
+      id="layout"
+      // border="1px solid red"
+      direction="column"
+      justifyContent="space-between"
+    >
+      <Box>
+        <Box display={{ base: "none", md: "block" }}>
+          <TextLogo />
+          <Navbar categories={categories} />
+        </Box>
 
-      {/* hides self when md breakpoint is hit */}
-      <Box display={{ base: "block", md: "none" }}>
-        <MobileNav categories={categories} />
-        <TextLogo />
-      </Box>
+        {/* hides self when md breakpoint is hit */}
+        <Box display={{ base: "block", md: "none" }}>
+          <MobileNav categories={categories} />
+          <TextLogo />
+        </Box>
 
-      <Box h="100%" border="1px solid green" position="relative">
-        {children}
+        <Box
+          h="100%"
+          // border="1px solid green"
+          position="relative"
+        >
+          {children}
+        </Box>
       </Box>
 
       <Text
-        border="1px solid green"
+        // border="1px solid green"
         fontSize="sm"
         mt="3rem"
         mb="1rem"
@@ -32,8 +46,43 @@ const layout = ({ children, categories }) => {
       >
         &copy; 2022 moneyandotherthings.com
       </Text>
-    </Box>
+    </Flex>
   );
+
+  // return (
+  //   <Box
+  //     overflowX="hidden"
+  //     h="100%"
+  //     minH="100vh"
+  //     id="layout"
+  //     border="1px solid red"
+  //   >
+  //     <Box display={{ base: "none", md: "block" }}>
+  //       <TextLogo />
+  //       <Navbar categories={categories} />
+  //     </Box>
+
+  //     {/* hides self when md breakpoint is hit */}
+  //     <Box display={{ base: "block", md: "none" }}>
+  //       <MobileNav categories={categories} />
+  //       <TextLogo />
+  //     </Box>
+
+  //     <Box h="100%" border="1px solid green" position="relative">
+  //       {children}
+  //     </Box>
+
+  //     <Text
+  //       border="1px solid green"
+  //       fontSize="sm"
+  //       mt="3rem"
+  //       mb="1rem"
+  //       textAlign="center"
+  //     >
+  //       &copy; 2022 moneyandotherthings.com
+  //     </Text>
+  //   </Box>
+  // );
 };
 
 export default layout;
