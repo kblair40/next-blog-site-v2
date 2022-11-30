143490c43e8ec36e7329071e17158a8eaaad121c
Kevin Blair <kblair40@gmail.com>
2022-11-30 07:54:37 -0600

    finish SubscriberForm

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -14,6 +14,7 @@ import axios from "axios";
 import SEO from "src/components/SEO";
 import FeaturedPost from "src/components/FeaturedPost";
 import ContactForm from "src/components/Forms/ContactForm";
+import SubscribeForm from "src/components/Forms/SubscribeForm";
 import AdditionalPosts from "./AdditionalPosts";
 import useAnalyticsEventTracker from "src/hooks/useAnalyticsEventTracker";
 
