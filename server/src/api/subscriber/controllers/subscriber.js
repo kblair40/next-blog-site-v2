8c1f0effc8b392d330803ff22446981b42a05877
Kevin Blair <kblair40@gmail.com>
2022-12-01 13:36:05 -0600

    Remove unused files and cleanup console.logs

--- server/src/api/subscriber/controllers/subscriber.js
+++ server/src/api/subscriber/controllers/subscriber.js
@@ -6,28 +6,4 @@
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController(
-  "api::subscriber.subscriber",
-  ({ strapi }) => ({
-    async sendEmail(event) {
-      const { result } = event;
-      console.log("\nAFTER CREATE EVENT RESULT lifecycle CONTROLLER:", result);
-
-      try {
-        await strapi.plugins["email"].services.email.send({
-          to: result.email,
-          from: "moneyandotherthings@gmail.com",
-          subject: "Thanks for subscribing!",
-          // text: "Thanks for subscribing!",
-          html: "Thanks for subscribing!",
-        });
-        console.log("EMAIL SUCCESSFULY SENT!");
-      } catch (e) {
-        console.log("ERROR SENDING EMAIL:", e);
-      }
-    },
-    // create(...args) {
-    //   console.log("CREATE ARGS:", args);
-    // },
-  })
-);
+module.exports = createCoreController("api::subscriber.subscriber");
--- server/src/index.js
+++ server/src/index.js
@@ -5,25 +5,5 @@ const boostrap = require("./bootstrap");
 module.exports = {
   async bootstrap() {
     await boostrap();
-
-    strapi.db.lifecycles.subscribe({
-      models: ["api::subscriber.subscribe"],
-      async afterCreate(event) {
-        const { result } = event;
-        console.log("\nAFTER CREATE EVENT RESULT lifecycle INDEX:", result);
-
-        try {
-          await strapi.plugins["email"].services.email.send({
-            to: result.email,
-            from: "moneyandotherthings@gmail.com",
-            subject: "Thanks for subscribing!",
-            html: "Thanks for subscribing!",
-          });
-          console.log("EMAIL SUCCESSFULY SENT!");
-        } catch (e) {
-          console.log("ERROR SENDING EMAIL:", e);
-        }
-      },
-    });
   },
 };
