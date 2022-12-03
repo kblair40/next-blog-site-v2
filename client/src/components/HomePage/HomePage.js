ad18b451ea3e7609a10eebd575c5017c45f5765f
Kevin Blair <kblair40@gmail.com>
2022-12-03 06:55:11 -0600

    Move animation to actual page component

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Flex, Box, Divider } from "@chakra-ui/react";
-import { motion, AnimatePresence } from "framer-motion";
 
 import SEO from "src/components/SEO";
 import FeaturedPost from "src/components/FeaturedPost";
@@ -15,30 +14,21 @@ const HomePage = ({ homepage, articles }) => {
   }
 
   return (
-    <AnimatePresence>
-      <motion.div
-        key="home"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-      >
-        <Box pb="2rem" minH="100vh" maxW="100vw" overflowX="hidden" px="1.5rem">
-          <SEO seo={homepage?.attributes.seo} />
+    <Box pb="2rem" minH="100vh" maxW="100vw" overflowX="hidden" px="1.5rem">
+      <SEO seo={homepage?.attributes.seo} />
 
-          <Flex direction="column" align="center" mt="90px" mb="2rem">
-            {featuredPost && <FeaturedPost featuredPost={featuredPost} />}
+      <Flex direction="column" align="center" mt="90px" mb="2rem">
+        {featuredPost && <FeaturedPost featuredPost={featuredPost} />}
 
-            <SubscribeForm />
-          </Flex>
+        <SubscribeForm />
+      </Flex>
 
-          <AdditionalPosts articles={articles} />
+      <AdditionalPosts articles={articles} />
 
-          <Divider borderColor="#303030" mb="2rem" />
+      <Divider borderColor="#303030" mb="2rem" />
 
-          <ContactForm />
-        </Box>
-      </motion.div>
-    </AnimatePresence>
+      <ContactForm />
+    </Box>
   );
 };
 
