d3b8e2c59f3b2aaa21c00313edfa8bc9c254cadd
Kevin Blair <kblair40@gmail.com>
2022-12-03 11:27:19 -0600

    Remove initialProps config for _app

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,9 +1,7 @@
-import App from "next/app";
 import Head from "next/head";
 import { ChakraProvider } from "@chakra-ui/react";
 import ReactGA from "react-ga";
 
-import { fetchAPI } from "src/utils/api";
 import theme from "src/utils/theme";
 import Layout from "src/components/Layout";
 import "../assets/css/style.css";
@@ -12,6 +10,7 @@ const TRACKING_ID = "UA-250380145-1";
 ReactGA.initialize(TRACKING_ID);
 
 const MyApp = ({ Component, pageProps }) => {
+  // console.log("\n\nPAGE PROPS:", pageProps, "\n\n");
   return (
     <>
       <Head></Head>
@@ -22,29 +21,6 @@ const MyApp = ({ Component, pageProps }) => {
       </ChakraProvider>
     </>
   );
-};
-
-// getInitialProps disables automatic static optimization for pages that don't
-// have getStaticProps. So article, category and home pages still get SSG.
-// Hopefully we can replace this with getStaticProps once this issue is fixed:
-// https://github.com/vercel/next.js/discussions/10949
-MyApp.getInitialProps = async (ctx) => {
-  // Calls page's `getInitialProps` and fills `appProps.pageProps`
-  const appProps = await App.getInitialProps(ctx);
-  // Fetch global site settings from Strapi
-  // const globalRes = await fetchAPI("/global", {
-  //   populate: {
-  //     favicon: "*",
-  //     defaultSeo: {
-  //       populate: "*",
-  //     },
-  //   },
-  // });
-  // console.log("GLOBAL RES:", globalRes);
-  // Pass the data to our page via props
-  // return appProps;
-  // console.log("\nAPP PROPS:", appProps);
-  return { ...appProps };
 };
 
 export default MyApp;
