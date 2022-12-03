05963dadd4068c519983e09577e36d9fd19a44ee
Kevin Blair <kblair40@gmail.com>
2022-12-03 17:54:15 -0600

    Move font loading back to _document

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { ChakraProvider } from "@chakra-ui/react";
 import ReactGA from "react-ga";
-import localFont from "@next/font/local";
+// import localFont from "@next/font/local";
 
 import theme from "src/utils/theme";
 import Layout from "src/components/Layout";
@@ -10,106 +10,106 @@ import "../assets/css/style.css";
 const TRACKING_ID = "UA-250380145-1";
 ReactGA.initialize(TRACKING_ID);
 
-export const nunito = localFont({
-  src: [
-    {
-      path: "./Nunito-Bold.ttf",
-      weight: "700",
-      style: "normal",
-      display: "swap",
-      fallback: ["system-ui"],
-    },
-    {
-      path: "./Nunito-Italic.ttf",
-      weight: "400",
-      style: "italic",
-      display: "swap",
-      fallback: ["system-ui"],
-    },
-    {
-      path: "./Nunito-Light.ttf",
-      weight: "300",
-      style: "normal",
-      display: "swap",
-      fallback: ["system-ui"],
-    },
-    {
-      path: "./Nunito-LightItalic.ttf",
-      weight: "300",
-      style: "italic",
-      display: "swap",
-      fallback: ["system-ui"],
-    },
-    {
-      path: "./Nunito-Medium.ttf",
-      weight: "500",
-      style: "normal",
-      display: "swap",
-      fallback: ["system-ui"],
-    },
-    {
-      path: "./Nunito-MediumItalic.ttf",
-      weight: "500",
-      style: "italic",
-      display: "swap",
-      fallback: ["system-ui"],
-    },
-    {
-      path: "./Nunito-Regular.ttf",
-      weight: "400",
-      style: "normal",
-      display: "swap",
-      fallback: ["system-ui"],
-    },
-    {
-      path: "./Nunito-SemiBold.ttf",
-      weight: "600",
-      style: "normal",
-      display: "swap",
-      fallback: ["system-ui"],
-    },
-  ],
-});
+// export const nunito = localFont({
+//   src: [
+//     {
+//       path: "./Nunito-Bold.ttf",
+//       weight: "700",
+//       style: "normal",
+//       display: "swap",
+//       fallback: ["system-ui"],
+//     },
+//     {
+//       path: "./Nunito-Italic.ttf",
+//       weight: "400",
+//       style: "italic",
+//       display: "swap",
+//       fallback: ["system-ui"],
+//     },
+//     {
+//       path: "./Nunito-Light.ttf",
+//       weight: "300",
+//       style: "normal",
+//       display: "swap",
+//       fallback: ["system-ui"],
+//     },
+//     {
+//       path: "./Nunito-LightItalic.ttf",
+//       weight: "300",
+//       style: "italic",
+//       display: "swap",
+//       fallback: ["system-ui"],
+//     },
+//     {
+//       path: "./Nunito-Medium.ttf",
+//       weight: "500",
+//       style: "normal",
+//       display: "swap",
+//       fallback: ["system-ui"],
+//     },
+//     {
+//       path: "./Nunito-MediumItalic.ttf",
+//       weight: "500",
+//       style: "italic",
+//       display: "swap",
+//       fallback: ["system-ui"],
+//     },
+//     {
+//       path: "./Nunito-Regular.ttf",
+//       weight: "400",
+//       style: "normal",
+//       display: "swap",
+//       fallback: ["system-ui"],
+//     },
+//     {
+//       path: "./Nunito-SemiBold.ttf",
+//       weight: "600",
+//       style: "normal",
+//       display: "swap",
+//       fallback: ["system-ui"],
+//     },
+//   ],
+// });
 
-export const playfair = localFont({
-  src: [
-    {
-      path: "./PlayfairDisplay-Bold.ttf",
-      weight: "700",
-      style: "normal",
-      display: "swap",
-      fallback: ["system-ui"],
-    },
-    {
-      path: "./PlayfairDisplay-ExtraBold.ttf",
-      weight: "800",
-      style: "normal",
-      display: "swap",
-      fallback: ["system-ui"],
-    },
-    {
-      path: "./PlayfairDisplay-Medium.ttf",
-      weight: "500",
-      style: "normal",
-      display: "swap",
-      fallback: ["system-ui"],
-    },
-    {
-      path: "./PlayfairDisplay-Regular.ttf",
-      weight: "400",
-      style: "normal",
-      display: "swap",
-      fallback: ["system-ui"],
-    },
-    {
-      path: "./PlayfairDisplay-SemiBold.ttf",
-      weight: "600",
-      style: "normal",
-      display: "swap",
-      fallback: ["system-ui"],
-    },
-  ],
-});
+// export const playfair = localFont({
+//   src: [
+//     {
+//       path: "./PlayfairDisplay-Bold.ttf",
+//       weight: "700",
+//       style: "normal",
+//       display: "swap",
+//       fallback: ["system-ui"],
+//     },
+//     {
+//       path: "./PlayfairDisplay-ExtraBold.ttf",
+//       weight: "800",
+//       style: "normal",
+//       display: "swap",
+//       fallback: ["system-ui"],
+//     },
+//     {
+//       path: "./PlayfairDisplay-Medium.ttf",
+//       weight: "500",
+//       style: "normal",
+//       display: "swap",
+//       fallback: ["system-ui"],
+//     },
+//     {
+//       path: "./PlayfairDisplay-Regular.ttf",
+//       weight: "400",
+//       style: "normal",
+//       display: "swap",
+//       fallback: ["system-ui"],
+//     },
+//     {
+//       path: "./PlayfairDisplay-SemiBold.ttf",
+//       weight: "600",
+//       style: "normal",
+//       display: "swap",
+//       fallback: ["system-ui"],
+//     },
+//   ],
+// });
 
 const MyApp = ({ Component, pageProps }) => {
   return (
