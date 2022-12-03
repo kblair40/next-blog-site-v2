51233ea66dd7fbe31a924b02e2e6cd4bf0cd2217
Kevin Blair <kblair40@gmail.com>
2022-12-03 16:25:28 -0600

    Add custom fonts

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { ChakraProvider } from "@chakra-ui/react";
 import ReactGA from "react-ga";
+import { Nunito, Playfair_Display } from "@next/font/google";
 
 import theme from "src/utils/theme";
 import Layout from "src/components/Layout";
@@ -11,12 +12,17 @@ ReactGA.initialize(TRACKING_ID);
 
 const MyApp = ({ Component, pageProps }) => {
   // console.log("\n\nPAGE PROPS:", pageProps, "\n\n");
+  const nunito = Nunito({ subsets: ["latin"] });
+  const playfair = Playfair_Display({ subsets: ["latin"] });
+
   return (
     <>
       <Head></Head>
       <ChakraProvider theme={theme} resetCSS={true}>
         <Layout>
-          <Component {...pageProps} />
+          <main className={`${nunito.className} ${playfair.className}`}>
+            <Component {...pageProps} />
+          </main>
         </Layout>
       </ChakraProvider>
     </>
