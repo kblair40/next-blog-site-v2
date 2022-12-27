bdeda4567a9a4990d07bb1118d7a62b70c819055
Kevin Blair <kblair40@gmail.com>
2022-12-27 15:47:36 -0500

    update todos / fix alignment of posts on home page

--- client/src/components/HomePage/AdditionalPosts/AdditionalPosts.js
+++ client/src/components/HomePage/AdditionalPosts/AdditionalPosts.js
@@ -40,7 +40,6 @@ const AdditionalPosts = ({ articles }) => {
       <Flex justify="center" flex={{ md: 1 }} py="1rem">
         <Box w="100%" maxW={{ md: "700px" }}>
           <Text mb="2rem" fontSize="2xl" letterSpacing={"5px"}>
-            {/* TRAIN OF THOUGHT */}
             RECENT POSTS
           </Text>
 
