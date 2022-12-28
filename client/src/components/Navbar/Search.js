7651a05dd9747c9d1a6eac98c777864d6c2d745d
Kevin Blair <kblair40@gmail.com>
2022-12-28 06:52:54 -0500

    Add Popover wrapper to search input

--- client/src/components/Navbar/Search.js
+++ client/src/components/Navbar/Search.js
@@ -28,39 +28,50 @@ const Search = () => {
   }, [asPath]);
 
   return (
-    <InputGroup
-      w="100%"
-      ml={{ base: "2rem", md: "-12px" }}
-      mr={{ base: "2rem", md: "unset" }}
-      maxW={{ base: "100%", sm: "200px", md: "120px", lg: "186px" }}
-      minW={{ base: "100px", md: "120px" }}
-      variant={{ base: "unstyled" }}
-      borderBottom="1px solid"
-      transition="border-color 0.2s"
-      borderColor="brand.lightgreen"
-      _hover={{ borderColor: "brand.darkgreen" }}
-      alignSelf={{ base: "flex-end", md: "center" }}
-    >
-      <InputLeftElement w="24px">
-        <SearchIcon fill="brand.lightgreen" boxSize="16px" />
-      </InputLeftElement>
-      <Input
-        pl="32px"
-        rounded="md"
-        borderColor="brand.lightgreen"
-        focusBorderColor="brand.lightgreen"
-        placeholder="Search..."
-        _placeholder={{
-          color: "brand.darkgreen",
-          opacity: "0.8",
-          fontSize: "14px",
-        }}
-        _hover={{ borderColor: "brand.darkgreen" }}
-        transition="border-color 0.2s"
-        onChange={(e) => setValue(e.target.value)}
-        value={value}
-      />
-    </InputGroup>
+    <Popover>
+      <PopoverTrigger>
+        <InputGroup
+          w="100%"
+          ml={{ base: "2rem", md: "-12px" }}
+          mr={{ base: "2rem", md: "unset" }}
+          maxW={{ base: "100%", sm: "200px", md: "120px", lg: "186px" }}
+          minW={{ base: "100px", md: "120px" }}
+          variant={{ base: "unstyled" }}
+          borderBottom="1px solid"
+          transition="border-color 0.2s"
+          borderColor="brand.lightgreen"
+          _hover={{ borderColor: "brand.darkgreen" }}
+          alignSelf={{ base: "flex-end", md: "center" }}
+        >
+          <InputLeftElement w="24px">
+            <SearchIcon fill="brand.lightgreen" boxSize="16px" />
+          </InputLeftElement>
+          <Input
+            pl="32px"
+            rounded="md"
+            borderColor="brand.lightgreen"
+            focusBorderColor="brand.lightgreen"
+            placeholder="Search..."
+            _placeholder={{
+              color: "brand.darkgreen",
+              opacity: "0.8",
+              fontSize: "14px",
+            }}
+            _hover={{ borderColor: "brand.darkgreen" }}
+            transition="border-color 0.2s"
+            onChange={(e) => setValue(e.target.value)}
+            value={value}
+          />
+        </InputGroup>
+      </PopoverTrigger>
+
+      <PopoverContent>
+        <PopoverArrow />
+        <PopoverHeader>Header</PopoverHeader>
+
+        <PopoverBody>{/*  */}</PopoverBody>
+      </PopoverContent>
+    </Popover>
   );
 };
 
