5e54c3719ffc6b1c68f4a3e81d8824271050395d
Kevin Blair <kblair40@gmail.com>
2022-12-29 15:28:24 -0600

    set default config for postgres db

--- server/config/database.js
+++ server/config/database.js
@@ -14,10 +14,30 @@
 //   },
 // });
 
+// const parse = require("pg-connection-string").parse;
+// const config = parse(process.env.DATABASE_URL);
+// console.log("\nDATABASE_URL:", process.env.DATABASE_URL);
+// // console.log("\nCONFIG:", config, "\n");
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: config.host,
+//       port: config.port,
+//       database: config.database,
+//       user: config.user,
+//       password: config.password,
+//       ssl: {
+//         rejectUnauthorized: false,
+//       },
+//     },
+//     debug: false,
+//   },
+// });
+
 const parse = require("pg-connection-string").parse;
 const config = parse(process.env.DATABASE_URL);
-console.log("\nDATABASE_URL:", process.env.DATABASE_URL);
-// console.log("\nCONFIG:", config, "\n");
 
 module.exports = ({ env }) => ({
   connection: {
