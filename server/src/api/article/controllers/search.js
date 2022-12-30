3b733e20bd1fc7c638dcf0dca13ea81e2078a3f9
Kevin Blair <kblair40@gmail.com>
2022-12-30 06:12:56 -0600

    Add Fuse instance for matching against article titles

--- server/src/api/article/controllers/search.js
+++ server/src/api/article/controllers/search.js
@@ -1,4 +1,5 @@
 "use strict";
+const Fuse = require("fuse.js");
 
 /**
  *  article search controller
@@ -10,25 +11,38 @@ const controller = createCoreController(
   "api::article.article",
   ({ strapi }) => {
     // console.log("STRAPI SERVICES:", strapi);
-    console.log("\n\nSTRAPI DB KEYS:", Object.keys(strapi.db));
-    console.log(
-      "\n\nSTRAPI DB MGR KEYS:",
-      Object.keys(strapi.db.entityManager)
-    );
+    // console.log("\n\nSTRAPI DB KEYS:", Object.keys(strapi.db));
+    // console.log(
+    //   "\n\nSTRAPI DB MGR KEYS:",
+    //   Object.keys(strapi.db.entityManager)
+    // );
     const service = strapi.service("api::article.article");
-    console.log("\n\nSTRAPI SERVICE:", service);
+    // console.log("\n\nSTRAPI SERVICE:", service);
     return {
       async search(ctx) {
         // console.log("\n\n\n\nCUSTOM ACTION CONTEXT:", ctx.params, "\n\n\n\n");
         try {
-          const searchTerm = ctx.params?.searchText || "";
-          console.log("\n\nSEARCH TEXT:", searchTerm, "\n\n");
+          const searchText = ctx.params?.searchText || "";
+          console.log("\n\nSEARCH TEXT:", searchText, "\n\n");
           const allArticles = await service.find();
-          console.log("\n\nALL ARTICLES:", allArticles, "\n\n");
-          // ctx.body.data = allArticles;
+          console.log("\n\nALL ARTICLES:", allArticles.results, "\n\n");
+
+          const fuse = new Fuse(allArticles.results, {
+            keys: ["title"],
+          });
+
+          const searchedArticles = fuse.search(searchText);
+          let titles = [];
+          if (searchedArticles && searchedArticles.length) {
+            titles = searchedArticles.map((art) => {
+              console.log("SEARCHED ART:", art);
+              return art.item.title;
+            });
+          }
+          console.log("\nSEARCHED ARTICLES:", titles, "\n\n");
+          return searchedArticles;
+
           return allArticles;
-          // ctx.body = allArticles;
-          // ctx.body = "ok";
         } catch (err) {
           console.log("\n\n\n\nSEARCH ERROR:", err, "\n\n\n\n\n");
           ctx.body = err;
