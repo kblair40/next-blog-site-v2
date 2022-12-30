fb067b776f817675a7ab4b25e9946703bc5203d4
Kevin Blair <kblair40@gmail.com>
2022-12-30 11:54:33 -0600

    Add state mgmt for homepage version being rendered

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Divider, Grid, GridItem } from "@chakra-ui/react";
 import dynamic from "next/dynamic";
 
@@ -28,27 +28,26 @@ const FeaturedResources = dynamic(
 );
 
 const HomePage = ({ homepage, articles }) => {
+  const [version, setVersion] = useState("original");
+
   let featuredPost;
   if (homepage && homepage.attributes?.featured_post) {
     featuredPost = homepage.attributes.featured_post.article.data;
   }
 
   return (
-    <Box
-      pt={{ base: "3rem", md: "5rem" }}
-      pb="2rem"
-      minH="100vh"
-      maxW="100vw"
-      w="100%"
-    >
-      <React.Suspense fallback={<div />}>
-        <SEO seo={homepage?.attributes.seo} />
-      </React.Suspense>
+    <React.Fragment>
+      {version === "original" ? (
+        <Box pb="2rem" minH="100vh" maxW="100vw" w="100%">
+          <React.Suspense fallback={<div />}>
+            <SEO seo={homepage?.attributes.seo} />
+          </React.Suspense>
 
-      <Grid
-        mb="2rem"
-        gridTemplateAreas={{
-          base: `
+          <Grid
+            mt={{ base: "60px", md: "90px" }}
+            mb="2rem"
+            gridTemplateAreas={{
+              base: `
           "feat-p"
           "sub"
           "recent"
@@ -56,61 +55,146 @@ const HomePage = ({ homepage, articles }) => {
           "div"
           "contact"
         `,
-          md: `
+              md: `
+            "feat-p feat-p"
+            "sub sub"
+            "recent feat-r"
+            "div div"
+            "contact contact"
+          `,
+            }}
+            gridTemplateColumns={{
+              base: "100%",
+              md: "auto minmax(240px, 300px)",
+            }}
+            w="100%"
+            gridTemplateRows={{
+              base: "429px 183px auto auto 1px 394px",
+              sm: "474px 183px auto auto 1px 394px",
+              md: "auto 183px auto 1px 394px",
+            }}
+            rowGap={{ base: "2.5rem", sm: "3rem" }}
+            columnGap={{ md: "2rem", lg: "4rem" }}
+            px={{ base: "1.5rem", sm: "2.5rem", lg: "5rem" }}
+            sx={{
+              "> *": {
+                // border: "1px solid skyblue",
+              },
+            }}
+          >
+            <GridItem area="feat-p">
+              <FeaturedPost featuredPost={featuredPost} />
+            </GridItem>
+
+            <GridItem area="sub">
+              <React.Suspense fallback={<Loading />}>
+                <SubscribeForm />
+              </React.Suspense>
+            </GridItem>
+
+            <GridItem area="feat-r">
+              <React.Suspense fallback={<div />}>
+                <FeaturedResources />
+              </React.Suspense>
+            </GridItem>
+
+            <GridItem area="recent">
+              <React.Suspense fallback={<Loading />}>
+                <AdditionalPosts articles={articles} />
+              </React.Suspense>
+            </GridItem>
+
+            <GridItem area="div">
+              <Divider borderColor="#303030" mb="2rem" />
+            </GridItem>
+
+            <GridItem area="contact">
+              <React.Suspense fallback={<Loading />}>
+                <ContactForm />
+              </React.Suspense>
+            </GridItem>
+          </Grid>
+        </Box>
+      ) : (
+        <Box
+          pt={{ base: "3rem", md: "5rem" }}
+          pb="2rem"
+          minH="100vh"
+          maxW="100vw"
+          w="100%"
+        >
+          <React.Suspense fallback={<div />}>
+            <SEO seo={homepage?.attributes.seo} />
+          </React.Suspense>
+
+          <Grid
+            mb="2rem"
+            gridTemplateAreas={{
+              base: `
+          "feat-p"
+          "sub"
+          "recent"
+          "feat-r"
+          "div"
+          "contact"
+        `,
+              md: `
             "feat-p feat-r"
             "sub sub"
             "recent contact"
           `,
-        }}
-        gridTemplateColumns={{
-          base: "auto",
-          md: "auto minmax(240px, 300px)",
-          lg: "auto minmax(300px, 350px)",
-          xl: "auto minmax(380px, 430px)",
-        }}
-        w="100%"
-        gridTemplateRows={{
-          base: "auto auto auto auto 1px auto",
-          sm: "auto auto auto auto 1px auto",
-          md: "auto auto auto",
-        }}
-        rowGap={{ base: "2.5rem", sm: "3.5rem" }}
-        columnGap={{ md: "2.5rem", lg: "4rem" }}
-        px={{ base: "1.5rem", sm: "2.5rem", lg: "5rem" }}
-      >
-        <GridItem area="feat-p">
-          <FeaturedPost featuredPost={featuredPost} />
-        </GridItem>
+            }}
+            gridTemplateColumns={{
+              base: "auto",
+              md: "auto minmax(240px, 300px)",
+              lg: "auto minmax(300px, 350px)",
+              xl: "auto minmax(380px, 430px)",
+            }}
+            w="100%"
+            gridTemplateRows={{
+              base: "auto auto auto auto 1px auto",
+              sm: "auto auto auto auto 1px auto",
+              md: "auto auto auto",
+            }}
+            rowGap={{ base: "2.5rem", sm: "3.5rem" }}
+            columnGap={{ md: "2.5rem", lg: "4rem" }}
+            px={{ base: "1.5rem", sm: "2.5rem", lg: "5rem" }}
+          >
+            <GridItem area="feat-p">
+              <FeaturedPost featuredPost={featuredPost} />
+            </GridItem>
 
-        <GridItem area="sub">
-          <React.Suspense fallback={<Loading />}>
-            <SubscribeForm py={{ base: "2rem", md: "3rem" }} />
-          </React.Suspense>
-        </GridItem>
+            <GridItem area="sub">
+              <React.Suspense fallback={<Loading />}>
+                <SubscribeForm py={{ base: "2rem", md: "3rem" }} />
+              </React.Suspense>
+            </GridItem>
 
-        <GridItem area="feat-r">
-          <React.Suspense fallback={<div />}>
-            <FeaturedResources />
-          </React.Suspense>
-        </GridItem>
+            <GridItem area="feat-r">
+              <React.Suspense fallback={<div />}>
+                <FeaturedResources />
+              </React.Suspense>
+            </GridItem>
 
-        <GridItem area="recent">
-          <React.Suspense fallback={<Loading />}>
-            <AdditionalPosts articles={articles} />
-          </React.Suspense>
-        </GridItem>
+            <GridItem area="recent">
+              <React.Suspense fallback={<Loading />}>
+                <AdditionalPosts articles={articles} />
+              </React.Suspense>
+            </GridItem>
 
-        <GridItem area="div" display={{ md: "none" }}>
-          <Divider borderColor="#303030" mb="2rem" />
-        </GridItem>
+            <GridItem area="div" display={{ md: "none" }}>
+              <Divider borderColor="#303030" mb="2rem" />
+            </GridItem>
 
-        <GridItem area="contact">
-          <React.Suspense fallback={<Loading />}>
-            <ContactForm />
-          </React.Suspense>
-        </GridItem>
-      </Grid>
-    </Box>
+            <GridItem area="contact">
+              <React.Suspense fallback={<Loading />}>
+                <ContactForm />
+              </React.Suspense>
+            </GridItem>
+          </Grid>
+        </Box>
+      )}
+    </React.Fragment>
   );
 };
 
