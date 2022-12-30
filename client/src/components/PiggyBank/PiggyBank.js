1cb97467d9f48412656bed05db5975a5e9e6a111
Kevin Blair <kblair40@gmail.com>
2022-12-30 14:02:39 -0600

    Finish piggy bank animation

--- client/src/components/PiggyBank/PiggyBank.js
+++ client/src/components/PiggyBank/PiggyBank.js
@@ -1,38 +1,47 @@
 import React from "react";
-
-import styles from "./PiggyBank.module.css";
+import { Heading, Box, Text } from "@chakra-ui/react";
 
 const PiggyBank = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.main_content}>
-        <div className={styles.animation_container}>
-          {/* <Animation /> */}
-          <div className="piggy-wrapper">
-            <div className="piggy-wrap">
-              <div className="piggy">
-                <div className="nose"></div>
-                <div className="mouth"></div>
-                <div className="ear"></div>
-                <div className="tail">
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                </div>
-                <div className="eye"></div>
-                <div className="hole"></div>
+    <Box>
+      <Text
+        as={Heading}
+        fontWeight="600"
+        textAlign="center"
+        color="brand.darkgreen"
+        fontSize={{ base: "40px", sm: "44px" }}
+      >
+        Coming Soon
+      </Text>
+
+      <Box
+        transform={{ base: "scale(0.5)", sm: "scale(0.6)" }}
+        mt={{ base: "-2.5rem" }}
+      >
+        <div className="piggy-wrapper">
+          <div className="piggy-wrap">
+            <div className="piggy">
+              <div className="nose"></div>
+              <div className="mouth"></div>
+              <div className="ear"></div>
+              <div className="tail">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
               </div>
+              <div className="eye"></div>
+              <div className="hole"></div>
             </div>
-            <div className="coin-wrap">
-              <div className="coin">$</div>
-            </div>
-            <div className="legs"></div>
-            <div className="legs back"></div>
           </div>
+          <div className="coin-wrap">
+            <div className="coin">$</div>
+          </div>
+          <div className="legs"></div>
+          <div className="legs back"></div>
         </div>
-      </div>
-    </div>
+      </Box>
+    </Box>
   );
 };
 
