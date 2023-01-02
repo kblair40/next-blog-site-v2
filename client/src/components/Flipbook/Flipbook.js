2b7f51644e6bf262a9f44b3c0299323489d6e7cd
Kevin Blair <kblair40@gmail.com>
2023-01-02 12:41:57 -0600

    Add skeleton for Flipbook component

---
+++ client/src/components/Flipbook/Flipbook.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+import Image from "next/image";
+
+const Flipbook = () => {
+  return <Box>Flipbook</Box>;
+};
+
+export default Flipbook;
--- client/src/pages/admin/AdminPage.js
+++ client/src/pages/admin/AdminPage.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { Box } from "@chakra-ui/react";
 
+import Flipbook from "src/components/Flipbook";
+
 const AdminPage = () => {
-  return <Box>AdminPage</Box>;
+  return (
+    <Box>
+      <Flipbook />
+    </Box>
+  );
 };
 
 export default AdminPage;
