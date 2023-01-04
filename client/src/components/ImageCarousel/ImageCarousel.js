7c823c757bec3383e7cee83bfe950abc5255378c
Kevin Blair <kblair40@gmail.com>
2023-01-04 13:35:53 -0600

    Add transitions for slide images

--- client/src/components/ImageCarousel/ImageCarousel.js
+++ client/src/components/ImageCarousel/ImageCarousel.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Box, useBreakpointValue, Flex } from "@chakra-ui/react";
+import { Box, useBreakpointValue, Flex, Button } from "@chakra-ui/react";
 import { motion, AnimatePresence } from "framer-motion";
+import Image from "next/image";
 
 const ImageCarousel = ({ imageUrls }) => {
   const [images, setImages] = useState();
@@ -31,7 +32,7 @@ const ImageCarousel = ({ imageUrls }) => {
   if (!images || !images.length) return null;
   return (
     <AnimatePresence>
-      <Flex justify="center">
+      <Flex justify="center" position="relative">
         <motion.div
           // className={styles.img}
           style={{
@@ -51,7 +52,37 @@ const ImageCarousel = ({ imageUrls }) => {
             x: { type: "spring", stiffness: 300, damping: 300 },
             opacity: { duration: 1 },
           }}
-        ></motion.div>
+        >
+          <Image src={images[slideIdx]} style={{ objectFit: "cover" }} fill />
+        </motion.div>
+        <Button
+          zIndex={1000000}
+          border="1px solid blue"
+          position="absolute"
+          size="sm"
+          top="50%"
+          transform="translateY(-50%)"
+          left={0}
+          onClick={() => {
+            if (slideIdx > 0) setSlideIdx((idx) => idx - 1);
+          }}
+        >
+          back
+        </Button>
+        <Button
+          zIndex={1000000}
+          border="1px solid blue"
+          position="absolute"
+          size="sm"
+          top="50%"
+          transform="translateY(-50%)"
+          right={0}
+          onClick={() => {
+            if (slideIdx < images.length - 1) setSlideIdx((idx) => idx + 1);
+          }}
+        >
+          Next
+        </Button>
       </Flex>
     </AnimatePresence>
   );
