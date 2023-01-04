9e66f1309fbc660726da4efaf9ec367e07c018ae
Kevin Blair <kblair40@gmail.com>
2023-01-04 16:40:43 -0600

    Smooth transition between images

--- client/src/components/ImageCarousel/ImageCarousel.js
+++ client/src/components/ImageCarousel/ImageCarousel.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Box, useBreakpointValue, Flex, Button } from "@chakra-ui/react";
+import { Box, Flex, Button } from "@chakra-ui/react";
 import { motion, AnimatePresence } from "framer-motion";
 import Image from "next/image";
 import { wrap } from "popmotion";
@@ -28,7 +28,6 @@ const variants = {
 // https://www.musebecodes.dev/blog/cloudinaryimagecarousel
 const ImageCarousel = ({ imageUrls }) => {
   const [images, setImages] = useState([]);
-  const [slideIdx, setSlideIdx] = useState(0);
   const [imageEls, setImageEls] = useState();
   const [[slide, direction], setSlide] = useState([0, 0]);
 
@@ -45,7 +44,14 @@ const ImageCarousel = ({ imageUrls }) => {
       const images = imageUrls.split(",").map((url) => url.trim());
       const imageEls = images.map((img, i) => {
         return (
-          <Image key={i} src={images[i]} style={{ objectFit: "cover" }} fill />
+          <Box position="relative" h="300px" w="100%">
+            <Image
+              key={i}
+              src={images[i]}
+              style={{ objectFit: "cover" }}
+              fill
+            />
+          </Box>
         );
       });
       setImages(images);
@@ -53,44 +59,31 @@ const ImageCarousel = ({ imageUrls }) => {
     }
   }, [imageUrls]);
 
-  // const carouselWidth = useBreakpointValue({
-  //   base: "300px",
-  //   sm: "400px",
-  //   md: "600px",
-  // });
-
-  // const carouselHeight = useBreakpointValue({
-  //   base: "200px",
-  //   sm: "233px",
-  //   md: "400px",
-  // });
-  const carouselWidth = "300px";
-  const carouselHeight = "200px";
-
   if (!images || !images.length) return null;
   return (
-    <Flex position="relative">
+    <Flex position="relative" justify="center">
       <AnimatePresence initial={false} custom={direction}>
         <motion.div
           style={{
-            height: carouselHeight,
-            // width: carouselWidth,
             width: "100%",
-            position: "relative",
-            border: "1px solid green",
-            margin: "0 auto",
+            // border: "2px solid green",
           }}
-          key={slideIdx}
+          key={slide}
+          src={images[imageIndex]}
           custom={direction}
           variants={variants}
           initial="enter"
           animate="center"
           exit="exit"
+          // transition={{
+          //   x: { duration: 0.2, ease: "easeIn" },
+          //   opacity: { duration: 0.2 },
+          // }}
           transition={{
-            x: { type: "spring", stiffness: 300, damping: 30 },
-            opacity: { duration: 0.2 },
+            // x: { type: "spring", stiffness: 300, damping: 30 },
+            x: { duration: 0.3, ease: "linear" },
+            opacity: { duration: 0.1 },
           }}
-          // custom={direction}
           // initial={{ x: parseInt(carouselWidth), opacity: 0 }}
           // animate={{ x: 0, opacity: 1 }}
           // exit={{ x: -parseInt(carouselWidth), opacity: 0 }}
@@ -106,13 +99,19 @@ const ImageCarousel = ({ imageUrls }) => {
           // layout
         >
           {/* {imageEls[slideIdx]} */}
-          {/* {imageEls[slide]} */}
+          {/* {imageEls[imageIndex]} */}
           {/* <Image src={images[slideIdx]} style={{ objectFit: "cover" }} fill /> */}
-          <Image src={images[imageIndex]} style={{ objectFit: "cover" }} fill />
+          <Box position="relative" h="300px" w="100%">
+            <Image
+              src={images[imageIndex]}
+              style={{ objectFit: "cover" }}
+              fill
+            />
+          </Box>
         </motion.div>
         <Button
           zIndex={100}
-          border="1px solid blue"
+          // border="1px solid blue"
           position="absolute"
           size="sm"
           top="50%"
@@ -127,7 +126,7 @@ const ImageCarousel = ({ imageUrls }) => {
         </Button>
         <Button
           zIndex={100}
-          border="1px solid blue"
+          // border="1px solid blue"
           position="absolute"
           size="sm"
           top="50%"
