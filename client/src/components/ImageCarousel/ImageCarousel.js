b54c449dc197014aab35f1eedb3b696ea2ea459e
Kevin Blair <kblair40@gmail.com>
2023-01-04 17:24:07 -0600

    Finish animation layout

--- client/src/components/ImageCarousel/ImageCarousel.js
+++ client/src/components/ImageCarousel/ImageCarousel.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Box, Flex, Button } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 import { motion, AnimatePresence } from "framer-motion";
 import Image from "next/image";
 import { wrap } from "popmotion";
@@ -33,7 +33,7 @@ const ImageCarousel = ({ imageUrls }) => {
 
   const imageIndex = wrap(0, images.length, slide);
 
-  const paginate = (newDirection) => {
+  const changeSlide = (newDirection) => {
     setSlide([slide + newDirection, newDirection]);
   };
 
@@ -42,105 +42,56 @@ const ImageCarousel = ({ imageUrls }) => {
       setImages(null);
     } else {
       const images = imageUrls.split(",").map((url) => url.trim());
-      const imageEls = images.map((img, i) => {
-        return (
-          <Box position="relative" h="300px" w="100%">
-            <Image
-              key={i}
-              src={images[i]}
-              style={{ objectFit: "cover" }}
-              fill
-            />
-          </Box>
-        );
-      });
       setImages(images);
-      setImageEls(imageEls);
     }
   }, [imageUrls]);
 
   if (!images || !images.length) return null;
   return (
-    <Flex position="relative" justify="center">
-      <AnimatePresence initial={false} custom={direction}>
-        <motion.div
-          style={{
-            width: "100%",
-            // border: "2px solid green",
-          }}
-          key={slide}
-          src={images[imageIndex]}
-          custom={direction}
-          variants={variants}
-          initial="enter"
-          animate="center"
-          exit="exit"
-          // transition={{
-          //   x: { duration: 0.2, ease: "easeIn" },
-          //   opacity: { duration: 0.2 },
-          // }}
-          transition={{
-            // x: { type: "spring", stiffness: 300, damping: 30 },
-            x: { duration: 0.3, ease: "linear" },
-            opacity: { duration: 0.1 },
-          }}
-          // initial={{ x: parseInt(carouselWidth), opacity: 0 }}
-          // animate={{ x: 0, opacity: 1 }}
-          // exit={{ x: -parseInt(carouselWidth), opacity: 0 }}
-          // initial={{ x: 300, opacity: 0 }}
-          // animate={{ x: 0, opacity: 1 }}
-          // exit={{ x: -300, opacity: 0 }}
-          // transition={{
-          //   x: { duration: 0.3, ease: "linear" },
-          //   opacity: { duration: 0.3, ease: "linear" },
-          //   // x: { type: "spring", stiffness: 300, damping: 300, duration: 1 },
-          //   // opacity: { duration: 1 },
-          // }}
-          // layout
-        >
-          {/* {imageEls[slideIdx]} */}
-          {/* {imageEls[imageIndex]} */}
-          {/* <Image src={images[slideIdx]} style={{ objectFit: "cover" }} fill /> */}
-          <Box position="relative" h="300px" w="100%">
-            <Image
-              src={images[imageIndex]}
-              style={{ objectFit: "cover" }}
-              fill
-            />
-          </Box>
-        </motion.div>
-        <Button
-          zIndex={100}
-          // border="1px solid blue"
-          position="absolute"
-          size="sm"
-          top="50%"
-          transform="translateY(-50%)"
-          left={0}
-          onClick={() => paginate(-1)}
-          // onClick={() => {
-          //   if (slideIdx > 0) setSlideIdx((idx) => idx - 1);
-          // }}
-        >
-          back
-        </Button>
-        <Button
-          zIndex={100}
-          // border="1px solid blue"
-          position="absolute"
-          size="sm"
-          top="50%"
-          transform="translateY(-50%)"
-          right={0}
-          onClick={() => paginate(1)}
-          // onClick={() => {
-          //   if (slideIdx < images.length - 1) setSlideIdx((idx) => idx + 1);
-          // }}
-        >
-          Next
-        </Button>
-      </AnimatePresence>
-    </Flex>
+    <AnimatePresence initial={false} custom={direction}>
+      <motion.div
+        key={slide}
+        src={images[imageIndex]}
+        custom={direction}
+        variants={variants}
+        initial="enter"
+        animate="center"
+        exit="exit"
+        transition={{
+          x: {
+            type: "tween",
+            duration: 0.3,
+            ease: "linear",
+          },
+          opacity: { duration: 0.2 },
+        }}
+      >
+        <Box position="absolute" w="100%" h="300px">
+          <Image src={images[imageIndex]} style={{ objectFit: "cover" }} fill />
+        </Box>
+      </motion.div>
+
+      <Button
+        onClick={() => changeSlide(-1)}
+        position="absolute"
+        top="50%"
+        transform="translateY(-50%)"
+        left={0}
+        border="1px solid red"
+      >
+        Prev
+      </Button>
+      <Button
+        onClick={() => changeSlide(1)}
+        position="absolute"
+        top="50%"
+        transform="translateY(-50%)"
+        right={0}
+        border="1px solid red"
+      >
+        Next
+      </Button>
+    </AnimatePresence>
   );
 };
 
--- client/src/components/ImageCarousel/index.js
+++ client/src/components/ImageCarousel/index.js
@@ -1,47 +1,13 @@
-// import { useState } from "react";
-// import { Flex, Button } from "@chakra-ui/react";
-// // import { wrap } from "popmotion";
+import { Box } from "@chakra-ui/react";
 
-// import ImageCarousel from "./ImageCarousel";
+import ImageCarousel from "./ImageCarousel";
 
-// const CarouselWrapper = ({ imageUrls }) => {
-//   const [slideIdx, setSlideIdx] = useState(0);
-//   let maxIdx = 0;
-//   if (imageUrls) {
-//     maxIdx = imageUrls.split(",").length - 1;
-//   }
-//   return (
-//     <Flex justify="center" position="relative" border="1px solid red">
-//       <ImageCarousel imageUrls={imageUrls} slideIdx={slideIdx} />
-//       <Button
-//         border="1px solid blue"
-//         position="absolute"
-//         size="sm"
-//         top="50%"
-//         transform="translateY(-50%)"
-//         left={0}
-//         onClick={() => {
-//           if (slideIdx > 0) setSlideIdx((idx) => idx - 1);
-//         }}
-//       >
-//         back
-//       </Button>
-//       <Button
-//         border="1px solid blue"
-//         position="absolute"
-//         size="sm"
-//         top="50%"
-//         transform="translateY(-50%)"
-//         right={0}
-//         onClick={() => {
-//           if (slideIdx < maxIdx) setSlideIdx((idx) => idx + 1);
-//         }}
-//       >
-//         Next
-//       </Button>
-//     </Flex>
-//   );
-// };
+const CarouselWrapper = ({ imageUrls }) => {
+  return (
+    <Box position="relative" h="300px">
+      <ImageCarousel imageUrls={imageUrls} />
+    </Box>
+  );
+};
 
-// export default CarouselWrapper;
-export { default } from "./ImageCarousel";
+export default CarouselWrapper;
