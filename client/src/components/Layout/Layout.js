a8e3c317f3171ebf21a293472addd9a5deac4999
Kevin Blair <kblair40@gmail.com>
2023-01-05 14:03:09 -0600

    Finish layout changes

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -18,91 +18,32 @@ const layout = ({ children, categories }) => {
 
       <Flex
         overflowX="hidden"
-        // overflowY="auto"
-        // h="100%"
+        h="100%"
         minH={{ base: "100vh - 56px", md: "100vh - 80px" }}
-        // border="2px solid green"
         id="layout"
         direction="column"
         justifyContent="space-between"
-        bg="brand.creme" // new
+        bg="brand.creme"
         position="relative"
       >
         <Flex
           direction="column"
           minH={{ base: "127x", md: "calc(100vh - 151px)" }}
-          border="1px solid red"
-          // h="100%"
+          bg="brand.creme"
         >
-          <Box
-            // h="100%"
-            // position="relative"
-            bg="brand.creme"
-            border="1px solid blue"
-            // minH={"calc(100vh - 151px)"}
-            // maxH={"calc(100vh - 151px)"}
-            // flex={1}
-          >
-            {children}
-          </Box>
+          <Box>{children}</Box>
         </Flex>
 
         <Footer />
       </Flex>
     </Box>
   );
-
-  // return (
-  //   <Box
-  //     overflowX="hidden"
-  //     h="100%"
-  //     minH="100vh"
-  //     id="layout"
-  //     border="1px solid red"
-  //   >
-  //     <Box display={{ base: "none", md: "block" }}>
-  //       <TextLogo />
-  //       <Navbar categories={categories} />
-  //     </Box>
-
-  //     {/* hides self when md breakpoint is hit */}
-  //     <Box display={{ base: "block", md: "none" }}>
-  //       <MobileNav categories={categories} />
-  //       <TextLogo />
-  //     </Box>
-
-  //     <Box h="100%" border="1px solid green" position="relative">
-  //       {children}
-  //     </Box>
-
-  //     <Text
-  //       border="1px solid green"
-  //       fontSize="sm"
-  //       mt="3rem"
-  //       mb="1rem"
-  //       textAlign="center"
-  //     >
-  //       &copy; 2022 moneyandotherthings.com
-  //     </Text>
-  //   </Box>
-  // );
 };
 
 export const Footer = () => {
   return (
-    <Center
-      minH="71px"
-      h="71px"
-      pt="1rem"
-      // border="1px solid orange"
-    >
-      <Text
-        // h="24px"
-
-        fontSize="sm"
-        // mb="1rem"
-        textAlign="center"
-      >
+    <Center minH="71px" h="71px" pt="1rem">
+      <Text fontSize="sm" textAlign="center">
         &copy; 2022 moneyandotherthings.com
       </Text>
     </Center>
--- client/src/pages/posts/[category]/index.js
+++ client/src/pages/posts/[category]/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Flex, Stack, Box } from "@chakra-ui/react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import { useRouter } from "next/router";
 import dayjs from "dayjs";
 
@@ -12,29 +12,16 @@ const Posts = ({ articles }) => {
   const { asPath } = useRouter();
 
   return (
-    <AnimatePresence>
-      <motion.div
-        key={asPath}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        // exit={{ opacity: 0 }}
-        style={{ paddingTop: "1.5rem" }}
-        layout
-      >
-        <Box
-          w="100%"
-          // h="100%"
-          // border="1px solid green"
-          // p="2rem 0 1rem"
-          // position="absolute"
-          // position="relative"
-          // h="max-content"
-          //
-        >
-          <ArticleList articles={articles} />
-        </Box>
-      </motion.div>
-    </AnimatePresence>
+    <motion.div
+      key={asPath}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      layout
+    >
+      <Box w="100%" pt="1.5rem">
+        <ArticleList articles={articles} />
+      </Box>
+    </motion.div>
   );
 };
 
@@ -47,39 +34,13 @@ const ArticleList = ({ articles }) => {
   };
 
   return (
-    <Flex
-      w="100%"
-      justify="center"
-      // h="100%"
-      px={{ base: "1rem", sm: "2rem" }}
-      // border="1px solid red"
-      // position="absolute"
-      // top="26px"
-      // border="1px solid blue"
-      // p="2rem 0 1rem"
-      // h={{}}
-    >
+    <Flex w="100%" justify="center" px={{ base: "1rem", sm: "2rem" }}>
       {!!articles && !!articles.length ? (
-        <Stack
-          w="100%"
-          // position="absolute"
-          // top="32px"
-          // px={{ base: "1rem", sm: "2rem" }}
-          // mt="2rem"
-          // position="absolute"
-          spacing="1.5rem"
-          // border="1px solid orange"
-          // px=""
-          // p="2rem 0 8rem"
-          // pb="3rem"
-        >
+        <Stack w="100%" spacing="1.5rem">
           {articles && articles.length
-            ? articles
-                .sort(sortArticles)
-                .concat(articles)
-                .map((article, i) => {
-                  return <Card article={article} key={i} />;
-                })
+            ? articles.sort(sortArticles).map((article, i) => {
+                return <Card article={article} key={i} />;
+              })
             : null}
         </Stack>
       ) : (
@@ -88,36 +49,6 @@ const ArticleList = ({ articles }) => {
     </Flex>
   );
 };
-
-// const ArticleList = ({ articles }) => {
-//   const sortArticles = (a, b) => {
-//     // console.log("A/B:", { a, b });
-//     const { createdAt: aCreatedAt } = a.attributes;
-//     const { createdAt: bCreatedAt } = b.attributes;
-//     // console.log("CREATED STAMPS:", { aCreatedAt, bCreatedAt });
-//     return dayjs(aCreatedAt).isBefore(dayjs(bCreatedAt)) ? 1 : -1;
-//   };
-
-//   if (!!articles && !!articles.length) {
-//     return (
-//       <Flex justify="center" px={{ base: "1rem", sm: "2rem" }}>
-//         <Stack mt="2rem" spacing="1.5rem">
-//           {articles && articles.length
-//             ? articles.sort(sortArticles).map((article, i) => {
-//                 return <Card article={article} key={i} />;
-//               })
-//             : null}
-//         </Stack>
-//       </Flex>
-//     );
-//   } else {
-//     return (
-//       <Flex justify="center" px={{ base: "1rem", sm: "2rem" }}>
-//         <PiggyBank />
-//       </Flex>
-//     );
-//   }
-// };
 
 export async function getStaticProps({ params }) {
   const categoriesRes = await fetchAPI("/categories", {
