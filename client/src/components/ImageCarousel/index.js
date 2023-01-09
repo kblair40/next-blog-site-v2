c8bd3b1e351df3dfa7a4cdb253854c363adeffde
Kevin Blair <kblair40@gmail.com>
2023-01-09 09:39:07 -0600

    Cleanup comments

--- client/src/components/ImageCarousel/index.js
+++ client/src/components/ImageCarousel/index.js
@@ -1,19 +1,3 @@
-// import { Box } from "@chakra-ui/react";
-
-// import ImageCarousel from "./ImageCarousel";
-
-// const CarouselWrapper = ({ imageUrls }) => {
-//   return (
-//     // <Box position="relative" h="100%" border="1px solid blue">
-//     <ImageCarousel imageUrls={imageUrls} />
-//     // </Box>
-//   );
-// };
-
-// export default CarouselWrapper;
-
-// export { default } from "./ImageCarousel";
-
 import ImageCarousel from "./ImageCarousel";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "src/utils/theme";
