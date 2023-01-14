5f479d235d1a850a963058eb4959a95c75a6ef7c
Kevin Blair <kblair40@gmail.com>
2023-01-14 07:45:35 -0600

    Add AdminContext

---
+++ client/src/store/AdminContext.js
@@ -0,0 +1,29 @@
+import { createContext, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+const AdminContext = createContext();
+
+const AdminProvider = ({ children }) => {
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  const { asPath } = useRouter();
+
+  useEffect(() => {
+    const isAdmin = localStorage.getItem("isAdmin");
+    setIsAdmin(isAdmin && isAdmin === "yes");
+  }, [asPath]);
+
+  return (
+    <AdminContext.Provider
+      value={{
+        isAdmin,
+      }}
+    >
+      {children}
+    </AdminContext.Provider>
+  );
+};
+
+export default AdminContext;
+
+export { AdminProvider };
