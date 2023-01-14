6d52c33207d8ccfc5527f508f128288d4ccc5c36
Kevin Blair <kblair40@gmail.com>
2023-01-14 06:43:28 -0600

    Add ArticleList helper component

--- client/src/pages/admin/drafts/DraftsPage.js
+++ client/src/pages/admin/drafts/DraftsPage.js
@@ -30,6 +30,34 @@ const DraftsPage = ({ articles }) => {
 
 export default DraftsPage;
 
+const ArticleList = ({ articles }) => {
+  const sortArticles = (a, b) => {
+    const { createdAt: aCreatedAt } = a.attributes;
+    const { createdAt: bCreatedAt } = b.attributes;
+    return dayjs(aCreatedAt).isBefore(dayjs(bCreatedAt)) ? 1 : -1;
+  };
+
+  if (!!articles && !!articles.length) {
+    return (
+      <Flex justify="center" px={{ base: "1rem", sm: "2rem" }}>
+        <Stack mt="2rem" spacing="1.5rem">
+          {articles && articles.length
+            ? articles.sort(sortArticles).map((article, i) => {
+                return <Card article={article} key={i} isPreview={true} />;
+              })
+            : null}
+        </Stack>
+      </Flex>
+    );
+  } else {
+    return (
+      <Flex justify="center" px={{ base: "1rem", sm: "2rem" }}>
+        <PiggyBank />
+      </Flex>
+    );
+  }
+};
+
 export async function getServerSideProps() {
   // docs on fetching in draft/preview publicationState
   // https://docs.strapi.io/developer-docs/latest/developer-resources/database-apis-reference/rest/filtering-locale-publication.html#publication-state
