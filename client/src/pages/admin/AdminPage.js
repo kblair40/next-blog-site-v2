6e655b0cfebea1eac93f8d5eadd0760198cd8ad4
Kevin Blair <kblair40@gmail.com>
2023-01-14 07:30:29 -0600

    Add/finish FlipbookPage in Admin

--- client/src/pages/admin/AdminPage.js
+++ client/src/pages/admin/AdminPage.js
@@ -2,12 +2,10 @@ import React from "react";
 import { Box } from "@chakra-ui/react";
 
 import Links from "src/components/Admin/Links";
-import Flipbook from "src/components/Flipbook";
 
 const AdminPage = () => {
   return (
     <Box h="calc(100vh - 80px)" w="100%" pt="2rem">
-      {/* <Flipbook /> */}
       <Links />
     </Box>
   );
--- client/src/pages/admin/flipbook/FlipbookPage.js
+++ client/src/pages/admin/flipbook/FlipbookPage.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Box } from "@chakra-ui/react";
+
+import Flipbook from "src/components/Flipbook";
 
 const FlipbookPage = () => {
-  return <div>FlipbookPage</div>;
+  return (
+    <Box h="calc(100vh - 80px)" w="100%" pt="2rem">
+      <Flipbook />
+    </Box>
+  );
 };
 
 export default FlipbookPage;
