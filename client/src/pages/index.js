24d51b68cc9dde434648de97306e6de074def544
Kevin Blair <kevin.blair@emory.edu>
2023-03-28 08:19:49 -0500

    Fix featured_posts not being included in fetch

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -43,7 +43,7 @@ export async function getStaticProps() {
         featured_post: {
           populate: { article: "*" },
         },
-        featured_post: {
+        featured_posts: {
           populate: { articles: "* " }
         }
       },
